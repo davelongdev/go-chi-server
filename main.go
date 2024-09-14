f4cb697ff33b55c058f45970239cbe38ee31c9d6
dave long <davelongdev@gmail.com>
2024-09-14 19:34:38 -0400

    refactor code - move functionality to new application package

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import(
-  "net/http"
   "fmt"
+  "context"
 
-  "github.com/go-chi/chi/v5"
-  "github.com/go-chi/chi/v5/middleware"
+  "github.com/davelongdev/go-chi-server/application"
 )
 
 func main() {
+  app := application.New()
 
-  router := chi.NewRouter()
-  router.Use(middleware.Logger)
-
-  router.Get("/hello", basicHandler)
-
-  server := &http.Server{
-    Addr: ":3000",
-    Handler: router,
+  err := app.Start(context.TODO())
+  if  err != nil {
+    fmt.Println("failed to start app", err)
   }
-
-  err := server.ListenAndServe()
-  if err != nil {
-    fmt.Println("failed to listen to server", err)
-  }
-}
-
-func basicHandler (w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("hello world"))
 }
