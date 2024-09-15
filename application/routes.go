58ad57c5544e61951433bad2fbf81cad5f590735
dave long <davelongdev@gmail.com>
2024-09-14 21:55:25 -0400

    add routes

--- application/routes.go
+++ application/routes.go
@@ -1,10 +1,12 @@
 package application
 
 import (
-	"net/http"
+  "net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+  "github.com/go-chi/chi/v5"
+  "github.com/go-chi/chi/v5/middleware"
+  
+  "github.com/davelongdev/go-chi-server/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -16,5 +18,17 @@ func loadRoutes() *chi.Mux {
     w.WriteHeader(http.StatusOK)
   })
 
+  router.Route("/orders", loadOrderRoutes)
+
   return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+  orderHandler := &handler.Order{}
+
+  router.Post("/", orderHandler.Create)
+  router.Get("/", orderHandler.List)
+  router.Get("/{id}", orderHandler.GetById)
+  router.Put("/{id}", orderHandler.UpdateById)
+  router.Delete("/{id}", orderHandler.DeleteById)
 }
